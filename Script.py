8b1887a7e1fa7371cfe0e4701ff1ade07153da8b
Aɴᴏɴʏᴍᴏᴜs <mukeshprajeen@gmail.com>
2023-10-04 14:13:32 +0530

    FRADU

--- Script.py
+++ Script.py
@@ -12,7 +12,7 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - AutoFilterBot is a open source project. 
-- Source - https://github.com/mukeshmoni/FILTEROBOT
+- Source - https://github.com/mukeshmoni/Filterobot
 
 <b>DEVS:</b>
 - <a href=https://t.me/kingofAtttitude>Team vampire</a>"""
