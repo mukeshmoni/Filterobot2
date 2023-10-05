7fc50277bd174fbf5b5da505e4b54441d888f9e8
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2023-10-05 10:00:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,7 +34,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://moniabi:moniabi@cluster0.uqjbfru.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "moniabi")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
